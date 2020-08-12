2745ad8cf925dd2d3b9c6c6f582b3daa6789586d
sanghuncho <sanghun.cho@isys.de>
2020-08-12 15:36:55 +0200

    Logger is added and extended with scheduleBid

--- src/main/java/reserve_to_bid/ScheduleBid.java
+++ src/main/java/reserve_to_bid/ScheduleBid.java
@@ -12,24 +12,24 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class ScheduleBid {
-    public static List<ScheduleBid> scheduleBidList = new ArrayList<>();
 
     private String ebayItemNumber;
     private String userid;
     private double bidValue;
     private String reserveToBidTimeKorea;
+    private String alternativeBidTimeKorea;
+    private String endTimeAuctionGER;
     private Timer timer;
-    //private TimerTask timerTask;
     
     public ScheduleBid(){
         
     }
     
-    public ScheduleBid(String ebayItemNumber, String userid, double bidValue, String reserveToBidTimeKorea, Timer timer){
+    public ScheduleBid(String ebayItemNumber, String userid, double bidValue, String endTimeAuctionGER, Timer timer){
         this.ebayItemNumber = ebayItemNumber;
         this.userid = userid;
         this.bidValue = bidValue;
-        this.reserveToBidTimeKorea = reserveToBidTimeKorea;
+        this.endTimeAuctionGER = endTimeAuctionGER;
         this.timer = timer;
     }
     
@@ -40,8 +40,11 @@ public class ScheduleBid {
                 System.out.println("starts autoBidding!");
             }
         };
+
+        reserveToBidTimeKorea = reserveToBidTimekorea(endTimeAuctionGER);
+        
         try {
-            timer.schedule(timerTask, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(reserveToBidTimeKorea));
+        	timer.schedule(timerTask, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(reserveToBidTimeKorea));
         } catch (ParseException e) {
             //logger
         }
@@ -53,13 +56,30 @@ public class ScheduleBid {
         return this;
     }
     
+    public String getEbayItemNumber() {
+        return this.ebayItemNumber;
+    }
+    
     public ScheduleBid setUserid(String userid) {
         this.userid = userid;
         return this;
     }
     
+    public String getUserid() {
+        return this.userid;
+    }
+    
+    public Timer getTimer() {
+        return this.timer;
+    }
+    
     public ScheduleBid setBidValue(double bidValue) {
         this.bidValue = bidValue;
+        return this;
+    }
+    
+    public ScheduleBid setEndTimeAuctionGER(String endTimeAuctionGER) {
+        this.endTimeAuctionGER = endTimeAuctionGER;
         return this;
     }
     
@@ -69,16 +89,32 @@ public class ScheduleBid {
     }
     
     public ScheduleBid build() {
-        ScheduleBid scheduleBid = new ScheduleBid(ebayItemNumber, userid, bidValue, reserveToBidTimeKorea, timer);
-        scheduleBidList.add(scheduleBid);
+        ScheduleBid scheduleBid = new ScheduleBid(ebayItemNumber, userid, bidValue, endTimeAuctionGER, timer);
+        ScheduleBidPlaner.scheduleBidList.add(scheduleBid);
         return scheduleBid;
     }
 
     public static void main(String [] args) throws ParseException {
-    	
-        
     }
-    
+
+    private static String reserveToBidTimekorea(String endtimeAuctionGER){
+        //String endTime = "2020-08-09T06:22:53.000Z";
+        ZonedDateTime zonedDateTimeEndAuction = ZonedDateTime.parse(endtimeAuctionGER);
+        Date auctionEndTime =  java.util.Date.from(zonedDateTimeEndAuction.toInstant());
+        Date auctionEndTimedate = new Date(auctionEndTime.getTime());
+        Format format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("입찰마감시간 독일: " + format.format(auctionEndTimedate));
+        
+        ZonedDateTime zonedDateTimeReservation = zonedDateTimeEndAuction.minusMinutes(5L);
+        Date reserveToBidTime =  java.util.Date.from(zonedDateTimeReservation.toInstant());
+        System.out.println("입찰예약시간 독일: " + format.format(reserveToBidTime));
+        
+        ZonedDateTime zonedDateTimeReservationKorea = zonedDateTimeReservation.plusHours(8L);
+        Date reserveToBidTimeKorea =  java.util.Date.from(zonedDateTimeReservationKorea.toInstant());
+        System.out.println("입찰예약시간 한국서버: " +  format.format(reserveToBidTimeKorea));
+        return format.format(reserveToBidTimeKorea);
+    }
+
     private static void testScheduleBid() throws ParseException {
         List<Timer> timerList = new ArrayList<>();
         Timer timer1 = new Timer();
@@ -86,11 +122,10 @@ public class ScheduleBid {
         TimerTask timerTask1 = new TimerTask() {
             public void run() {
                 System.out.println("runnable task1");
-                //autoBidding starts
+                //autoBidding algorithmus starts
             }
         };
         timer1.schedule(timerTask1, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2020-08-07 11:15:10"));
-
 
         Timer task2 = new Timer();
         Timer task3 = new Timer();
--- src/main/java/reserve_to_bid/ScheduleBidPlaner.java
+++ src/main/java/reserve_to_bid/ScheduleBidPlaner.java
@@ -2,7 +2,26 @@ package reserve_to_bid;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ScheduleBidPlaner {
+	private static final Logger LOGGER = LogManager.getLogger();
     public static List<ScheduleBid> scheduleBidList = new ArrayList<>();
+    
+    public void cancelScheduleBid(String ebayItemNumber, String userid) {
+    	Objects.nonNull(ebayItemNumber);
+    	Objects.nonNull(userid);
+    	Optional<ScheduleBid> result = scheduleBidList
+    			.stream()
+    			.parallel()
+    			.filter(bid -> bid.getEbayItemNumber() == ebayItemNumber && bid.getUserid() == userid).findAny();
+    	
+    	result.ifPresentOrElse(
+    			resullt -> result.get().getTimer().cancel(), 
+    			() -> LOGGER.error("Scheduled bid is not found and not canceled - EbayItemNumber: " + ebayItemNumber + "/" + "userid: " + userid)
+    	);
+    }
 }
